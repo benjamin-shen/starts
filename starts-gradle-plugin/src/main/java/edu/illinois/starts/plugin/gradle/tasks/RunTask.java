805163793b107371a33161d5bc90807779790531
Benjamin Shen <bfs45@cornell.edu>
2022-11-27 21:33:12 -0500

    fix npe in run task

--- starts-gradle-plugin/src/main/java/edu/illinois/starts/plugin/gradle/tasks/RunTask.java
+++ starts-gradle-plugin/src/main/java/edu/illinois/starts/plugin/gradle/tasks/RunTask.java
@@ -129,8 +129,8 @@ public class RunTask extends DiffTask {
             dynamicallyUpdateExcludes(null);
             // Make nonAffected empty so dependencies can be updated
             nonAffectedTests.clear();
-            Writer.writeClassPath(cpString, artifactsDir);
-            Writer.writeJarChecksums(testDependencyElements, artifactsDir, jarCheckSums);
+            Writer.writeClassPath(cpString, getArtifactsDir());
+            Writer.writeJarChecksums(testDependencyElements, getArtifactsDir(), jarCheckSums);
         } else if (retestAll) {
             // Force retestAll but compute changes and affected tests
             setChangedAndNonaffected();
